aa0ac7137c38c40817f6460866419a56d94e1be9
ashepard03 <ashepard@appdev.yearup.org>
2023-04-25 10:13:54 -0400

    Updated loops

--- src/main/java/com/workshop/NeighborhoodLibraryUI.java
+++ src/main/java/com/workshop/NeighborhoodLibraryUI.java
@@ -1,14 +1,12 @@
 package com.workshop;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class NeighborhoodLibraryUI {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner userInput = new Scanner(System.in);
 
-        boolean done = false;
-
+        boolean done = true;
         do {
             System.out.println("Welcome to Java Section 10 Library. Please select an option: ");
             //prompt user to select what screen they want to view
@@ -23,47 +21,64 @@ public class NeighborhoodLibraryUI {
                     Book[] bookInventory = BookInventory.bookInventory;
                     for (Book i : bookInventory) {
                         if (i.isCheckedOut()) continue;
-                        System.out.println(i.getId() + " " + i.getTitle() + " " + i.getIsbn());
-
-                        System.out.println("Would you like to check out a book? (Yes/No)");
-                        String answer = userInput.nextLine().trim();
-                        // if yes, prompt them for book id and name
-                        switch (answer) {
-                            case "Yes":
-                                System.out.println("Please provide the ID of the book you would like to check out.");
-                                int bookId = userInput.nextInt();
-                                userInput.nextLine();
-                                for (Book b : bookInventory) {
-                                    if (b.getId() == bookId) {
-                                        System.out.println("Please enter you name: ");
-                                        String name = userInput.nextLine();
-                                        b.checkOut(name);
-                                        System.out.println("You have checked out the book " + b.getTitle());
-                                    }
+                        System.out.println(i.getId() + " / " + i.getTitle() + " / " + i.getIsbn());
+                    }
+                    //ask the user to select a book or return to home screen
+                    System.out.println("Would you like to check out a book? (Yes/No)");
+                    String answer = userInput.nextLine();
+                    // if yes, prompt them for book id and name
+                    switch (answer) {
+                        case "Yes":
+                            System.out.println("Please provide the ID of the book you would like to check out.");
+                            int bookId = userInput.nextInt();
+                            userInput.nextLine();
+                            for (Book b : bookInventory) {
+                                if (b.getId() == bookId) {
+                                    System.out.println("Please enter you name: ");
+                                    String name = userInput.nextLine();
+                                    b.checkOut(name);
+                                    System.out.println("You have checked out the book " + b.getTitle());
                                 }
-                                break;
-                            case "No":
-                                System.out.println("Returning to home screen");
-                        }
+                            }Thread.sleep(1000);
+                            main(null);
+                        case "No":
+                            System.out.println("Returning to home screen");
+                            Thread.sleep(1000);
+                            main(null);
                     }
                     break;
                 case 2:
+                    System.out.println("--------Checked Out Books--------");
                     bookInventory = BookInventory.bookInventory;
                     for (Book i : bookInventory) {
                         if (!i.isCheckedOut()) continue;
                         System.out.println(i.getId() + " " + i.getTitle());
                     }
-                    break;
+                    System.out.println("Would you like to check in a book (c) or return to the home screen (x)");
+                    String userResponse = userInput.nextLine().trim();
+                    switch (userResponse) {
+                        case "c":
+                            System.out.println("Please provide the book id of the book you would like to check in");
+                            int bookId = userInput.nextInt();
+                            userInput.nextLine();
+                            for (Book b : bookInventory)
+                                if (b.getId() == bookId) {
+                                    b.checkIn();
+                                    System.out.println("You have checked in the book " + b.getTitle());
+                                    System.out.println("Returning to the library home screen" );
+                                    Thread.sleep(1000);
+                                    main(null);
+                                }
+                        case "x":
+                            System.out.println("Returning to the library home screen");
+                            break;
+                    }
                 case 3:
                     System.out.println("Thank you for visiting the Java Section 10 Library!");
-                    done = true;
                     break;
+                default:
+                    System.out.println("Please Try again");
             }
         } while (!done);
-            // check out the book
-        //If the user selected to view checked out books display them
-            //ask the user to check in a book (c) or return to home screen (x)
-            //prompt them for book id
-            //check in book
     }
 }
